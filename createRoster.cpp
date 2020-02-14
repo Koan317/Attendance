8764e9262ea39b8281e2ad93e80aae936e516ca9
Koan <koan317@qq.com>
2020-02-15 01:26:42 +0800

    1.1 file available
    1.1 file available

---
+++ createRoster.cpp
@@ -0,0 +1,24 @@
+﻿#ifndef ROSTER
+#define ROSTER
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+vector<string> createRoster()
+{
+	ifstream fin("Roster.txt");
+	vector<string> roster;
+	if (!fin.is_open())
+		cerr << "Error opening file!" << endl;
+	else
+	{
+		string name;
+		while (getline(fin, name))
+			roster.push_back(name);
+		fin.close();
+	}
+	return roster;
+}
+#endif //ROSTER
